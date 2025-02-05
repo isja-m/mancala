9b8d9cb536fd304f28b0e0543e9a478963c8b204
Isja Mannens <isja-m@hotmail.com>
2025-02-05 16:40:32 +0100

    Jacoco.

---
+++ backend/persistence/src/test/java/mancala/persistence/MancalaRepositoryTest.java
@@ -0,0 +1,29 @@
+package mancala.persistence;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashMap;
+import mancala.domain.IMancala;
+import mancala.domain.Mancala;
+import mancala.persistence.IMancalaRepository;
+import mancala.persistence.MancalaRepository;
+
+
+public class MancalaRepositoryTest {
+    private IMancalaRepository repository;
+
+    @BeforeEach
+    private void setUp() {
+        repository = new MancalaRepository();
+    }
+
+    @Test
+    public void canLoadSavedGameToRepository() {
+        IMancala mancala = new Mancala("Allice", "Bob");
+        repository.save("key", mancala);
+        assertEquals(mancala.saveGameState(), repository.get("key").saveGameState());
+    }
+
+}
